085f5d1adc1666df7538e596ccea18f762d44bd2
neosyk-net <runerdycastillo@gmail.com>
2025-12-11 22:21:55 -0500

    added sign in as guest!

--- tomato-clone/src/components/LoginPopup/LoginPopup.jsx
+++ tomato-clone/src/components/LoginPopup/LoginPopup.jsx
@@ -1,35 +1,60 @@
-import React, { useState } from 'react'
-import './LoginPopup.css'
-import { assets } from '../../assets/assets'
+import React, { useState } from "react";
+import "./LoginPopup.css";
+import { assets } from "../../assets/assets";
 
-const LoginPopup = ({setShowLogin}) => {
+const LoginPopup = ({ setShowLogin }) => {
+  const [currState, setCurrState] = useState("Login");
 
-    const [currState, setCurrState] = useState("Login")
+  const handleContinueAsGuest = () => {
+    setShowLogin(false);
+  };
 
   return (
-    <div className='login-popup'>
+    <div className="login-popup">
       <form action="" className="login-popup-container">
         <div className="login-popup-title">
-            <h2>{currState}</h2>
-            <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
+          <h2>{currState}</h2>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt=""
+          />
         </div>
         <div className="login-popup-inputs">
-            {currState === "Login" ? <></> : <input type="text" placeholder='Your Name' required /> }
-            <input type="email" placeholder='Your Email' required />
-            <input type="password" placeholder='Password' required/>
+          {currState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Your Name" required />
+          )}
+          <input type="email" placeholder="Your Email" required />
+          <input type="password" placeholder="Password" required />
         </div>
         <button>{currState === "Sign Up" ? "Create Account" : "Login"}</button>
         <div className="login-popup-condition">
-            <input type="checkbox" required/>
-            <p>By continuing, I agree to the terms of use & privacy policy</p>
+          <input type="checkbox" required />
+          <p>By continuing, I agree to the terms of use & privacy policy</p>
         </div>
-        {currState === "Login"
-        ? <p>Create a New Account? <span onClick={() => setCurrState("Sign Up")}>Click Here</span></p>
-        : <p>Already have an Account? <span onClick={() => setCurrState("Login")}>Login Here</span></p>
-        }
+
+        {currState === "Login" && (
+          <p className="guest-login" onClick={handleContinueAsGuest}>
+            Continue as <span>Guest</span>
+          </p>
+        )}
+
+        {currState === "Login" ? (
+          <p>
+            Create a New Account?{" "}
+            <span onClick={() => setCurrState("Sign Up")}>Click Here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an Account?{" "}
+            <span onClick={() => setCurrState("Login")}>Login Here</span>
+          </p>
+        )}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPopup
+export default LoginPopup;
